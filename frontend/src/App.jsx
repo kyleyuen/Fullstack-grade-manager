b9fa09c6fe7e6f92d57bae67c7a251cbfeaa99ef
Kyle Yuen <45528039+kyleyuen@users.noreply.github.com>
2025-09-27 21:30:43 -0700

    Made initial commit to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+const [students, setStudents] = useState([]);
+const [status, setStatus] = useState('idle');
+
+useEffect(() => {
+  async function fetchStudents() {
+    setStatus('loading');
+    try {
+      const res = await fetch('/students');
+      const data = await res.json();
+      setStudents(data);
+      setStatus('idle');
+    }
+    catch {
+      setStatus('error');
+    }
+  }
+  fetchStudents();
+}, []);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="p-4">
+      {/* UI for grade manager */}
+      {status === 'loading' && <p>Loading students...</p>}
+      {status === 'error' && <p>Error loading students.</p>}
+
+      {students.map(students => (
+        <div
+          key={student.id} 
+          className="border shadow-md rounded-lg p-4 mb-2"
+        >
+          <p><strong>Name:</strong> {student.student_name}</p>
+          <p><strong>Grade:</strong> {student.grade}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
